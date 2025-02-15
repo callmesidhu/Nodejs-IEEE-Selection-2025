ce6551047f4c8ed9b283c8d80ea7c33f214dec46
S SIDHARTH <sidharthpunalur@gmail.com>
2025-02-16 02:46:43 +0530

    signup recieved on db not response generated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require("body-parser");
 
 var loginRouter = require('./routes/login');
 var signupRouter = require('./routes/signup');
@@ -22,12 +23,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,42 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var db = require("../configs/dbconnection");
+var bcrypt = require("bcrypt");
 
+router.get("/", function (req, res, next) {
+  res.render("signup");
+});
 
-router.get('/', function(req, res, next) {
-  res.render('signup');
+router.post("/", async function (req, res, next) {
+  const { fullname, password, email } = req.body;
+
+  console.log("📥 Received Data:", { fullname, email });
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("🔐 Hashed Password:", hashedPassword);
+
+    // Insert into MySQL
+    let sql = "INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)";
+    db.query(sql, [fullname, email, hashedPassword], (err, result) => {
+      if (err) {
+        console.error("❌ Database Error:", err);
+        return res.status(500).send("Database error!");
+      }
+
+      console.log("✅ User added successfully:", result);
+      
+      // Debugging redirection
+      console.log("➡️ Redirecting to /login");
+
+      return res.redirect("/login");
+    });
+
+  } catch (error) {
+    console.error("❌ Unexpected Error:", error);
+    return res.status(500).send("Something went wrong!");
+  }
 });
 
 module.exports = router;
