e2d4d71ea153b766e0a4f3648717a04b783956da
S SIDHARTH <sidharthpunalur@gmail.com>
2025-02-17 03:36:13 +0530

    employee session added

--- routes/employee.js
+++ routes/employee.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('employee');
+router.get('/', function (req, res, next) {
+    if (req.session.user) {
+        if (req.session.user.type === 'employee') {
+            res.render('employee', { message: `You are an ${req.session.user.type}` });
+        } else {
+            res.send(`<script>alert("You are an ${req.session.user.type}"); window.location.href='/';</script>`);
+        }
+    } else {
+        res.send(`<script>alert("You need to log in first!"); window.location.href='/login';</script>`);
+    }
 });
 
 module.exports = router;
