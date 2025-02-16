2f91ed960618855aade64cde468b7dedee9fbcc8
S SIDHARTH <sidharthpunalur@gmail.com>
2025-02-17 03:40:27 +0530

    admin session added

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-  res.render('admin');
+router.get('/', function (req, res, next) {
+  if (req.session.user) {
+      if (req.session.user.type === 'admin') {
+          res.render('admin', { message: `You are an ${req.session.user.type}` });
+      } else {
+          res.send(`<script>alert("You are an ${req.session.user.type}"); window.location.href='/';</script>`);
+      }
+  } else {
+      res.send(`<script>alert("You need to log in first!"); window.location.href='/login';</script>`);
+  }
 });
 
 module.exports = router;
