c5c574a1453584b01f1cc6c2d423f3d79e5a1692
S SIDHARTH <sidharthpunalur@gmail.com>
2025-02-17 03:38:21 +0530

    manager session added

--- routes/manager.js
+++ routes/manager.js
@@ -22,8 +22,18 @@ const teams = [
   }
 ];
 
-router.get('/', function(req, res, next) {
-  res.render('manager', { teams: teams }); // Pass teams to the EJS template
+router.get('/', function (req, res, next) {
+    if (req.session.user) {
+        if (req.session.user.type === 'manager') {
+            res.render('manager', { message: `You are an ${req.session.user.type}` });
+        } else {
+            res.send(`<script>alert("You are an ${req.session.user.type}"); window.location.href='/';</script>`);
+        }
+    } else {
+        res.send(`<script>alert("You need to log in first!"); window.location.href='/login';</script>`);
+    }
 });
 
 module.exports = router;
+
+
